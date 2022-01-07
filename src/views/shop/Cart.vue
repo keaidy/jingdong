e071e48623ca690ad647e5691d4307d3303a6dcc
maomao <1294949339@qq.com>
2022-01-08 00:58:19 +0800

    增加cat功能响应

--- src/views/shop/Cart.vue
+++ src/views/shop/Cart.vue
@@ -1,13 +1,57 @@
 <template>
-  <div class="cart">
-    <div class="cart__icon">
-      <img src="./img/cart.png" alt="" class="cart__icon__img" />
-      <div class="cart__icon__tag">{{ total }}</div>
+  <div class="check">
+    <div class="product">
+      <template  v-for="item in productlist" :key="item._id">
+        <div class="product__item" v-if="item.count > 0">
+          <img
+            src="http://www.dell-lee.com/imgs/vue3/near.png"
+            alt=""
+            class="product__item__img"
+          />
+          <div class="product__item__list">
+            <h4 class="product__item__title">{{ item.name }}</h4>
+            <p class="product__item__sales">{{ item.sales }}</p>
+            <p class="product__item__price">
+              <span class="product__item__yen">&yen;</span>{{ item.price }}
+              <span class="product__item__origin"
+                >&yen;{{ item.oldPrice }}</span
+              >
+            </p>
+          </div>
+          <div class="product__number">
+            <span
+              class="product__number__minus"
+              @click="
+                () => {
+                  changeCartItemInfo(shopId, item._id, item, -1);
+                }
+              "
+              >-</span
+            >
+            {{item.count || 0 }}
+            <span
+              class="product__number__plus"
+              @click="
+                () => {
+                  changeCartItemInfo(shopId, item._id, item, 1);
+                }
+              "
+              >+</span
+            >
+          </div>
+        </div>
+      </template>
     </div>
-    <div class="cart__price">
-      总计：<span class="cart__price__check">&yen;{{ price }}</span>
+    <div class="cart">
+      <div class="cart__icon">
+        <img src="./img/cart.png" alt="" class="cart__icon__img" />
+        <div class="cart__icon__tag">{{ total }}</div>
+      </div>
+      <div class="cart__price">
+        总计：<span class="cart__price__check">&yen;{{ price }}</span>
+      </div>
+      <div class="cart__go">去结算</div>
     </div>
-    <div class="cart__go">去结算</div>
   </div>
 </template>
 
@@ -15,47 +59,54 @@
 import { computed } from "vue";
 import { useStore } from "vuex";
 import { useRoute } from "vue-router";
+import { useCommonCartEffect } from "./commonCartEffect";
 
 const useCartEffect = () => {
   const store = useStore();
   const route = useRoute();
   const shopId = route.params.id;
-  const {cartList} = store.state
+  const { cartList } = store.state;
   const total = computed(() => {
-    const productList = cartList[shopId]
-    let count = 0
+    const productList = cartList[shopId];
+    let count = 0;
     if (productList) {
       for (let i in productList) {
-        const product = productList[i]
-        count += product.count
+        const product = productList[i];
+        count += product.count;
       }
     }
-   return count
-  })
+    return count;
+  });
   const price = computed(() => {
     const productList = cartList[shopId];
     let count = 0;
     if (productList) {
       for (let i in productList) {
-        const product = productList[i]
-        count += (product.price * product.count);
+        const product = productList[i];
+        count += product.price * product.count;
       }
     }
     return count.toFixed(2);
   });
-  return { total, price };
+  const productlist = computed(() => {
+    const productList = cartList[shopId] || [];
+    return productList;
+  });
+  return { total, price, productlist };
 };
+
 export default {
   name: "Cart",
   setup() {
-    const { total, price } = useCartEffect();
-    return { total, price };
+    const { total, price, productlist } = useCartEffect();
+    const { changeCartItemInfo } = useCommonCartEffect();
+    return { total, price, changeCartItemInfo, productlist };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.cart {
+.check {
   position: absolute;
   left: 0;
   right: 0;
@@ -63,6 +114,8 @@ export default {
   height: 0.49rem;
   background-color: #fff;
   box-shadow: 0 -1px 1px 0 #f1f1f1;
+}
+.cart {
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -110,6 +163,75 @@ export default {
     font-size: 0.14rem;
     line-height: 0.49rem;
     text-align: center;
+  }
+}
+.product {
+  overflow-y: scroll;
+  flex:1;
+  padding-left: 0.16rem;
+  &__item {
+    padding-bottom: 0.15rem;
+    border-bottom: 1px solid #f1f1f1;
+    margin-bottom: 0.15rem;
+    display: flex;
+    &__img {
+      width: 0.68rem;
+      height: 0.68rem;
+    }
+    &__list {
+      flex: 1;
+      padding-left: 0.16rem;
+    }
+    &__title {
+      font-size: 0.14rem;
+      color: #333;
+    }
+    &__sales {
+      font-size: 0.14rem;
+      margin: 0.08rem 0;
+      color: #333;
+    }
+    &__price {
+      font-size: 14px;
+      color: #e93b3b;
+    }
+    &__yuan {
+      font-size: 0.12rem;
+    }
+    &__origin {
+      margin-left: 0.06rem;
+      line-height: 0.2rem;
+      font-size: 0.12rem;
+      color: #999;
+      text-decoration: line-through;
+    }
+  }
+  &__number {
+    width: 0.76rem;
+    margin-right: 0.18rem;
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+    font-size: 0.16rem;
+    &__minus,
+    &__plus {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 0.2rem;
+      height: 0.2rem;
+      border-radius: 50%;
+      font-size: 0.2rem;
+    }
+    &__minus {
+      border: 1px solid #666;
+      margin-right: 0.114rem;
+    }
+    &__plus {
+      background-color: #0091ff;
+      margin-left: 0.114rem;
+      color: #fff;
+    }
   }
 }
 </style>
--- src/views/shop/Content.vue
+++ src/views/shop/Content.vue
@@ -59,8 +59,8 @@
 <script>
 import { reactive, toRefs, ref, watchEffect } from "vue";
 import { useRoute } from "vue-router";
-import { useStore } from "vuex";
 import { get } from "../../utils/request";
+import {useCommonCartEffect} from './commonCartEffect'
 
 const categories = [
   { name: "全部商品", tab: "all" },
@@ -94,19 +94,7 @@ const useCurrentListEffect = (currentTab, shopId) => {
   return { list };
 };
 //购物车逻辑
-const useCartEffect = () => {
-  const store = useStore();
-  const { cartList } = toRefs(store.state);
-  const changeCartItemInfo = (shopId, productId, productInfo, num) => {
-    store.commit("changeCartItemInfo", {
-      shopId,
-      productId,
-      productInfo,
-      num,
-    });
-  };
-  return { cartList, changeCartItemInfo };
-};
+
 
 export default {
   name: "Content",
@@ -115,7 +103,7 @@ export default {
     const shopId = route.params.id;
     const { handleCategoryClick, currentTab } = useTabEffect();
     const { list } = useCurrentListEffect(currentTab, shopId);
-    const { changeCartItemInfo, cartList } = useCartEffect();
+    const { changeCartItemInfo, cartList } = useCommonCartEffect();
     return {
       list,
       shopId,
